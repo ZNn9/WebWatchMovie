508dc8afec88af7baffea8cd6b4e6d3f2fe1ace1
2180606387-LeHongAnh <139670615+LeHongAnh-2180606387@users.noreply.github.com>
2024-07-06 17:20:50 +0700

    git commit "[add]"
    Autocomplete cá nhân hóa

--- src/main/java/com/BLUEGREEN/WebWatchMovie/service/MovieService.java
+++ src/main/java/com/BLUEGREEN/WebWatchMovie/service/MovieService.java
@@ -1,14 +1,20 @@
 package com.BLUEGREEN.WebWatchMovie.service;
 
 import com.BLUEGREEN.WebWatchMovie.model.Movie;
+import com.BLUEGREEN.WebWatchMovie.model.Tag;
 import com.BLUEGREEN.WebWatchMovie.model.TagMovieDetails;
+import com.BLUEGREEN.WebWatchMovie.model.UserDetailsMovieFollow;
 import com.BLUEGREEN.WebWatchMovie.repository.MovieRepository;
 import com.BLUEGREEN.WebWatchMovie.repository.TagMovieDetailsRepository;
 import com.BLUEGREEN.WebWatchMovie.repository.TagRepository;
+import com.BLUEGREEN.WebWatchMovie.repository.UserDetailsMovieFollowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,6 +25,9 @@ public class MovieService {
 
     @Autowired
     private TagRepository tagRepository;
+
+    @Autowired
+    private UserDetailsMovieFollowRepository userDetailsMovieFollowRepository;
 
     @Autowired
     private TagMovieDetailsRepository tagMovieDetailsRepository;
@@ -56,6 +65,28 @@ public class MovieService {
     public List<String> searchMovies(String query) {
         List<Movie> movies = movieRepository.findByNameContainingIgnoreCase(query);
         return movies.stream().map(Movie::getName).collect(Collectors.toList());
+    }
+
+    public List<Movie> recommendMovies(int userId) {
+        List<UserDetailsMovieFollow> userMovies = userDetailsMovieFollowRepository.findByUserId(userId);
+
+        Set<Tag> userTags = new HashSet<>();
+        for (UserDetailsMovieFollow userMovie : userMovies) {
+            List<TagMovieDetails> tagDetails = tagMovieDetailsRepository.findByMovieId(userMovie.getMovie().getIdMovie());
+            for (TagMovieDetails tagDetail : tagDetails) {
+                userTags.add(tagDetail.getTag());
+            }
+        }
+
+        Set<Movie> recommendedMovies = new HashSet<>();
+        for (Tag tag : userTags) {
+            List<TagMovieDetails> tagDetails = tagMovieDetailsRepository.findByTagId(tag.getIdTag());
+            for (TagMovieDetails tagDetail : tagDetails) {
+                recommendedMovies.add(tagDetail.getMovie());
+            }
+        }
+
+        return new ArrayList<>(recommendedMovies);
     }
 
 }
