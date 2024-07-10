41a18fe3bd25aefb7cb24c4bd597f67dce613607
baolaptrinh <109140427+baolaptrinh@users.noreply.github.com>
2024-07-11 02:54:25 +0700

    alo

--- src/main/java/com/BLUEGREEN/WebWatchMovie/controller/MovieController.java
+++ src/main/java/com/BLUEGREEN/WebWatchMovie/controller/MovieController.java
@@ -1,5 +1,12 @@
 package com.BLUEGREEN.WebWatchMovie.controller;
 
+<<<<<<< Updated upstream
+=======
+import com.BLUEGREEN.WebWatchMovie.model.Movie;
+import com.BLUEGREEN.WebWatchMovie.service.MovieService;
+import com.BLUEGREEN.WebWatchMovie.model.Episode;
+import com.BLUEGREEN.WebWatchMovie.service.EpisodeService;
+>>>>>>> Stashed changes
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MovieController {
 
+<<<<<<< Updated upstream
+=======
+    @Autowired
+    private EpisodeService episodeService;
+
+    @Autowired
+    private MovieService movieService;
+
+>>>>>>> Stashed changes
     @GetMapping("/")
-    public String showMovies() {
-        return "/user/index";
+    public String showMovies(Model model) {
+        List<Movie> movies = movieService.getAllMovies();
+        model.addAttribute("movies", movies);
+        return "user/index";
     }
 }
