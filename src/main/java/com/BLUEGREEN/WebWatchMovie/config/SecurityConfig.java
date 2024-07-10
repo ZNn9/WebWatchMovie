c9626d08357e14780339d303ae9ff00dda51019a
NguyenDaiHiep_2180605809 <nightz2622@gmail.com>
2024-07-10 14:44:00 +0700

    Update SecurityConfig.java

--- src/main/java/com/BLUEGREEN/WebWatchMovie/config/SecurityConfig.java
+++ src/main/java/com/BLUEGREEN/WebWatchMovie/config/SecurityConfig.java
@@ -12,9 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -106,19 +103,5 @@ public class SecurityConfig {
                         .failureUrl("/user/login?error")
                 )
                 .build();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
     }
 }
