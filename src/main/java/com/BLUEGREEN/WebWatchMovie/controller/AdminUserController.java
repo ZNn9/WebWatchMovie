2b998c1f20b9179e33e63a586bcb76a12acf2ec0
NguyenDaiHiep_2180605809 <nightz2622@gmail.com>
2024-07-10 17:53:50 +0700

    Create AdminUserController.java

---
+++ src/main/java/com/BLUEGREEN/WebWatchMovie/controller/AdminUserController.java
@@ -0,0 +1,12 @@
+package com.BLUEGREEN.WebWatchMovie.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminUserController {
+    
+}
